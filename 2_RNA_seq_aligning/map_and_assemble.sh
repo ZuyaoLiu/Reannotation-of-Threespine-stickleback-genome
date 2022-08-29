c785bb3bad1c383e22ab4eb73d4872e589688778
Zuyao Liu <49354864+ZuyaoLiu@users.noreply.github.com>
2022-08-29 22:29:29 +0200

    Create map_and_assemble.sh

---
+++ 2_RNA_seq_aligning/map_and_assemble.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Please make sure Hisat 2.2.1, Stringtie 2.2.1 and samtools are installed in your environment
+#read_file_list formant:
+#Sample_name"\t"trimmed_R1_reads"\t"trimmed_R2_reads
+##### 
+#Commond "bash map_and_assemble.sh tissue index read_file_list theads Library_type(PEorSE) partition"
+#E.g: map_and_assemble.sh  Liver ../index/Ga read.list 10 PE bdw
+
+name=$1
+index=$2
+read_file_list=$3
+threads=$4
+lib=$5
+partition=$6
+
+num=`cat $read_file_list |wc -l`
+cat mapping.sh |sed -e "s!index!$index!g" -e "s!tissue!$name!g" -e "s!read_file_list!$read_file_list!g" -e "s!threads!$threads!g" -e "s!library!$lib!g" -e "s!final!$num!g" > mapping.submit.sh
+cat assembling_transcripts.sh | sed -e "s/tissue/$name/g"  -e "s/threads/$threads/g" > assembling_transcripts.submit.sh
+
+sbatch --wait -p $partition mapping.submit.sh
+sbatch --wait -p $partition assembling_transcripts.submit.sh
